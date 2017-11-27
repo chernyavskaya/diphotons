d79ed45d1cdeeb6b4e897cbd77572be6bc835311
Francesco <micheli@cern.ch>
2017-11-27 15:54:08 +0100

    final setup for optimization

--- Utils/macro/run_all_nodes.sh
+++ Utils/macro/run_all_nodes.sh
@@ -1,35 +1,37 @@
-
+#!/bin/bash
 #which=""
 ## _mjj_nodr
 
+infile="/mnt/t3nfs01/data01/shome/micheli/HHbbgg_ETH_devel/outfiles/20170828_optimizedCuts_noDR_minDRGJet/Total_preselection_diffNaming_transformedMVA.root"
+
+now=$(date '+%Y%m%d_%H%M%S')
+echo $now
+
 #for node in inputs/node_*.root; do name=$(basename $node | sed 's%.root%%'); 
 for node in jsons_hh/node_*.json; do name=$(basename $node | sed 's%.json%%'); 
-echo $node
+echo "------------------------------------------STARTING NODE -----------------------------------------------------"
 echo $name
-## for node in inputs$which/node_SM.root; do name=$(basename $node | sed 's%.root%%'); 
-###original command
-#    ./categoryOptimizationMultiDim.py --load optimize_hh.json --infile inputs$which/background_no_fakes.root --sigfile $node  --label $name -o www/higgs/hh/cat_opt$which/no_fakes_no_SMH_MXcategories_$name; 
+echo "-------------------------------------------------------------------------------------------------------------"
 ##hh version
-    ./categoryOptimizationMultiDim.py --load jsons_hh/optimize_hh_MVA.json --load $node --infile infiles/Total_preselection_diffNaming_transformedMVA.root --sigfile infiles/Total_preselection_diffNaming_transformedMVA.root  --label test_SM -o outPlots/out_test_${name}_transformed/
+## MVA boundaries optimization with fixed cut in MX and Mjj
+#    ./categoryOptimizationMultiDim.py --load jsons_hh/optimize_hh_MVA.json --load $node --infile ${infile} --sigfile ${infile}  --label test_SM -o outPlots/out_${now}_${name}_transformed_MVA/
 
+## MX boundaries optimization with optimized MVA boundaries and fixed cut in Mjj
+#    ./categoryOptimizationMultiDim.py --load jsons_hh/optimize_hh_MX.json --load $node --infile ${infile} --sigfile ${infile}  --label test_SM -o outPlots/out_${now}_${name}_transformed_MX/
 
-    ## ./categoryOptimizationMultiDim.py --load optimize_hh.json --infile inputs$which/background_no_fakes.root --sigfile $node  --label $name -o www/higgs/hh/cat_opt$which/no_fakes_no_SMH_3x3_$name &
-    ## ./categoryOptimizationMultiDim.py --load optimize_hh.json --infile inputs$which/background_no_fakes.root --sigfile $node  --label $name -o www/higgs/hh/cat_opt$which/no_fakes_no_SMH_3x3_mjj_cut_$name &
+## MVA boundaries optimization with optimized MX boundaries and fixed cut in Mjj
+#    ./categoryOptimizationMultiDim.py --load jsons_hh/optimize_hh_MVA_MXboundaries.json --load $node --infile ${infile} --sigfile ${infile}  --label test_SM -o outPlots/out_${now}_${name}_transformed_MVA_step2/
 
-
-    ## for cat in cat012 cat345 cat567 cat036 cat147 cat258; do
-    ## 	./categoryOptimizationMultiDim.py --load optimize_mjj.json --infile inputs$which/background_no_fakes.root --sigfile $node  --label $name -o www/higgs/hh/cat_opt$which/no_fakes_no_SMH_3x3_mjj_cut_${cat}_$name --load $cat.json &
-    ## done
-    ## wait
-    
-#    for cat in cat0 cat1 cat2 cat3 cat4 cat5 cat6 cat7 cat8; do
-#    	./categoryOptimizationMultiDim.py --load optimize_mjj.json --infile inputs$which/background_no_fakes.root --sigfile $node  --label $name -o www/higgs/hh/cat_opt$which/no_fakes_no_SMH_3x3_mjj_cut_${cat}_$name --load $cat.json &
-#    done
+#for cat in cat0; do
+    for cat in cat0 cat1 cat2 cat3 cat4 cat5 cat6 cat7 cat8; do
+    	./categoryOptimizationMultiDim.py --load jsons_hh/optimize_mjj.json --infile ${infile} --sigfile ${infile}  --label $name -o outPlots/out_${now}_${name}_${cat}_transformed_mjj/ --load $cat.json --load $node
+    done
 #    wait
+
 
 
 done
 
-scp -r outPlots lxplus.cern.ch:/afs/cern.ch/user/m/micheli/www/plots/HHBBGGOptimization/
+scp -r outPlots/out_${now}* lxplus.cern.ch:/afs/cern.ch/user/m/micheli/www/plots/HHBBGGOptimization/outPlots/
 
 wait
