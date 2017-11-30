d0311298308db811a16184085dc52bd4dea32290
Francesco <micheli@cern.ch>
2017-11-30 16:17:36 +0100

    final commands

--- Utils/macro/run_all_nodes.sh
+++ Utils/macro/run_all_nodes.sh
@@ -12,6 +12,15 @@ for node in jsons_hh/node_*.json; do name=$(basename $node | sed 's%.json%%');
 echo "------------------------------------------STARTING NODE -----------------------------------------------------"
 echo $name
 echo "-------------------------------------------------------------------------------------------------------------"
+
+## if you need to skip some nodes
+#if [[ $name == *node_10* || $name == *node_11* || $name == *node_12* || $name == *node_13*|| $name == *node_2* || $name == *node_3* || $name == *node_4* || $name == *node_5* ]];
+#then
+#continue
+#fi
+
+
+
 ##hh version
 ## MVA boundaries optimization with fixed cut in MX and Mjj
 #    ./categoryOptimizationMultiDim.py --load jsons_hh/optimize_hh_MVA.json --load $node --infile ${infile} --sigfile ${infile}  --label test_SM -o outPlots/out_${now}_${name}_transformed_MVA/
@@ -19,19 +28,27 @@ echo "--------------------------------------------------------------------------
 ## MX boundaries optimization with optimized MVA boundaries and fixed cut in Mjj
 #    ./categoryOptimizationMultiDim.py --load jsons_hh/optimize_hh_MX.json --load $node --infile ${infile} --sigfile ${infile}  --label test_SM -o outPlots/out_${now}_${name}_transformed_MX/
 
+
+
 ## MVA boundaries optimization with optimized MX boundaries and fixed cut in Mjj
 #    ./categoryOptimizationMultiDim.py --load jsons_hh/optimize_hh_MVA_MXboundaries.json --load $node --infile ${infile} --sigfile ${infile}  --label test_SM -o outPlots/out_${now}_${name}_transformed_MVA_step2/
 
+#mjj cut optimization
 #for cat in cat0; do
-    for cat in cat0 cat1 cat2 cat3 cat4 cat5 cat6 cat7 cat8; do
-    	./categoryOptimizationMultiDim.py --load jsons_hh/optimize_mjj.json --infile ${infile} --sigfile ${infile}  --label $name -o outPlots/out_${now}_${name}_${cat}_transformed_mjj/ --load $cat.json --load $node
-    done
+for cat in cat0 cat1 cat2 cat3 cat4 cat5 cat6 cat7 cat8; do
+    ./categoryOptimizationMultiDim.py --load jsons_hh/optimize_mjj.json --infile ${infile} --sigfile ${infile}  --label $name -o outPlots/out_${now}_${name}_${cat}_transformed_mjj/ --load $cat.json --load $node
+done
+
+#mjj cut reopt on one cat
+#for cat in cat2; do
+#./categoryOptimizationMultiDim.py --load jsons_hh/optimize_mjj.json --infile ${infile} --sigfile ${infile}  --label $name -o outPlots/out_${now}_${name}_${cat}_transformed_mjj/ --load ${cat}_nevts.json --load $node
+#done
 #    wait
 
 
 
 done
 
-scp -r outPlots/out_${now}* lxplus.cern.ch:/afs/cern.ch/user/m/micheli/www/plots/HHBBGGOptimization/outPlots/
+#scp -r outPlots/out_${now}* lxplus.cern.ch:/afs/cern.ch/user/m/micheli/www/plots/HHBBGGOptimization/outPlots/
 
 wait
