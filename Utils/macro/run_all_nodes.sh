2d98020f3fc7f67e6e28708d6b4e5190b067276f
Nadezda Chernyavskaya (ETHZ) [nchernya] <nchernya@t3ui02.psi.ch>
2019-04-17 15:34:58 +0200

    running optimization of large BDT first time with Mjj

--- Utils/macro/run_all_nodes.sh
+++ Utils/macro/run_all_nodes.sh
@@ -3,7 +3,7 @@
 ## _mjj_nodr
 
 #infile="/mnt/t3nfs01/data01/shome/micheli/HHbbgg_ETH_devel/outfiles/20170828_optimizedCuts_noDR_minDRGJet/Total_preselection_diffNaming_transformedMVA.root"
-infile="/mnt/t3nfs01/data01/shome/nchernya/HHbbgg_ETH_devel/outfiles/20180416/20180416_ggHHbbgg_DiPhotonJets_diffNaming.root"
+infile="/work/nchernya/HHbbgg_ETH_devel/outfiles/20190321_2017_2016_for_optimization/Total_preselection_diffNaming_2016_2017_21_03_2019_transformedMVA.root"
 #infile="/t3home/micheli/HHbbgg_ETH_20190128/HHbbgg_ETH/Training/output_files/20190128_test/Total_preselection_diffNaming_transformedMVA.root"
 #infile="/t3home/micheli/HHbbgg_ETH_20190128/HHbbgg_ETH/Training/output_files/20190201_optimized/Total_preselection_diffNaming_transformedMVA.root"
 
@@ -11,7 +11,7 @@ now=$(date '+%Y%m%d_%H%M%S')
 echo $now
 
 #for node in inputs/node_*.root; do name=$(basename $node | sed 's%.root%%'); 
-for node in jsons_hh_SM/node_*.json; do name=$(basename $node | sed 's%.json%%'); 
+for node in jsons_hh_Mjj/node_*.json; do name=$(basename $node | sed 's%.json%%'); 
 echo "------------------------------------------STARTING NODE -----------------------------------------------------"
 echo $name
 echo "-------------------------------------------------------------------------------------------------------------"
@@ -26,16 +26,15 @@ echo "--------------------------------------------------------------------------
 
 #######################################hh version
 ## MVA boundaries optimization with fixed cut in MX and Mjj
-    ./categoryOptimizationMultiDim.py --load jsons_hh_SM/optimize_hh_MVA.json --load $node --infile ${infile} --sigfile ${infile}  --label test_SM -o outPlots/out_${now}_${name}_transformed_MVA/
+#    ./categoryOptimizationMultiDim.py --load jsons_hh_Mjj/optimize_hh_MVA.json --load $node --infile ${infile} --sigfile ${infile}  --label mixNodes -o outPlots/out_${now}_${name}_transformed_MVA/
 
 ## MX boundaries optimization with optimized MVA boundaries and fixed cut in Mjj
-#    ./categoryOptimizationMultiDim.py --load jsons_hh_SM/optimize_hh_MX.json --load $node --infile ${infile} --sigfile ${infile}  --label test_SM -o outPlots/out_${now}_${name}_transformed_MX/
+#    ./categoryOptimizationMultiDim.py --load jsons_hh_Mjj/optimize_hh_MX.json --load $node --infile ${infile} --sigfile ${infile}  --label mixNodes -o outPlots/out_${now}_${name}_transformed_MX/
 
 
 
 ## MVA boundaries optimization with optimized MX boundaries and fixed cut in Mjj
-#    ./categoryOptimizationMultiDim.py --load jsons_hh_SM/optimize_hh_MVA_MXboundaries.json --load $node --infile ${infile} --sigfile ${infile}  --label test_SM -o outPlots/out_${now}_${name}_transformed_MVA_step2/
-#    ./categoryOptimizationMultiDim.py --load jsons_hh/optimize_hh_MVA_MXboundaries.json --load $node --infile ${infile} --sigfile ${infile}  --label test_SM -o outPlots/out_${now}_${name}_transformed_MVA_step2/
+    ./categoryOptimizationMultiDim.py --load jsons_hh_Mjj/optimize_hh_MVA_MXboundaries.json --load $node --infile ${infile} --sigfile ${infile}  --label mixNodes -o outPlots/out_${now}_${name}_transformed_MVA_step2/
 
 #mjj cut optimization
 #for cat in cat0; do
